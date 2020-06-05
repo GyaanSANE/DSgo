2dd605c7b33657ee747ccb8d593fe60a3a6ebfc5
GyaanSANE <abhigyan.rex@gmail.com>
2020-06-06 04:37:25 +0530

    added graph node

--- src/dataStructures/node/GraphNode.java
+++ src/dataStructures/node/GraphNode.java
@@ -10,6 +10,11 @@ public class GraphNode {
 	private int weight;
 	private boolean isVisited;
 	private GraphNode parent;
+	
+	public GraphNode(String name, int index) {
+		this.name = name;
+		this.index = index;
+	}
 	public String getName() {
 		return name;
 	}
