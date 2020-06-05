e272db5bf221e4add6a86d6a1d6470bae48447db
GyaanSANE <abhigyan.rex@gmail.com>
2020-06-06 02:41:46 +0530

    added graph node

---
+++ src/dataStructures/node/GraphNode.java
@@ -0,0 +1,50 @@
+package dataStructures.node;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+
+	private String name;
+	private int index;
+	private ArrayList<GraphNode> neighbours = new ArrayList<GraphNode>();
+	private int weight;
+	private boolean isVisited;
+	private GraphNode parent;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public ArrayList<GraphNode> getNeighbours() {
+		return neighbours;
+	}
+	public void setNeighbours(ArrayList<GraphNode> neighbours) {
+		this.neighbours = neighbours;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public boolean isVisited() {
+		return isVisited;
+	}
+	public void setVisited(boolean isVisited) {
+		this.isVisited = isVisited;
+	}
+	public GraphNode getParent() {
+		return parent;
+	}
+	public void setParent(GraphNode parent) {
+		this.parent = parent;
+	}
+		
+}
