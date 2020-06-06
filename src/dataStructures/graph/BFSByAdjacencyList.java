c23cd954453dc03b8af0277ac43ad1c2e3cab5b6
GyaanSANE <abhigyan.rex@gmail.com>
2020-06-07 02:01:37 +0530

    bfs by AdjList

---
+++ src/dataStructures/graph/BFSByAdjacencyList.java
@@ -0,0 +1,54 @@
+package dataStructures.graph;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import dataStructures.node.GraphNode;
+
+public class BFSByAdjacencyList {
+	
+	ArrayList<GraphNode> nodeList;
+	
+	public BFSByAdjacencyList(ArrayList<GraphNode> nodeList) {
+		this.nodeList = nodeList;
+	}
+	
+	public void bfs() {
+		for(GraphNode node : nodeList) {
+			if(!node.isVisited()) {
+				visit(node);
+			}
+		}
+	}
+	
+	
+	
+	
+	private void visit(GraphNode node) {
+		Queue<GraphNode> queue = new LinkedList<>();
+		queue.add(node);
+		while(!queue.isEmpty()) {
+			GraphNode curr = queue.remove();
+			curr.setVisited(true);
+			System.out.print(curr.getName()+" ");
+			for(GraphNode neighbour : curr.getNeighbours()) {
+				if(!neighbour.isVisited()) {
+					queue.add(neighbour);
+					neighbour.setVisited(true);
+				}
+			}
+		}
+		
+	}
+
+	
+
+	public void addUndirectedEdge(int i,int j) {
+		i--;
+		j--;
+		nodeList.get(i).getNeighbours().add(nodeList.get(j));
+		nodeList.get(j).getNeighbours().add(nodeList.get(i));
+	}
+	
+
+}
